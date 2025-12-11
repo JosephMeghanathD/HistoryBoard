aa5ce07cb05322ca77bdde09c1f49d32352ebd19
Joseph Hit Hard <joseph.meghanath@gmail.com>
2025-12-11 14:18:00 -0600

    Initial Commit

---
+++ HistoryBoard/HistoryBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryBoardApp.swift
+//  HistoryBoard
+//
+//  Created by Joseph Meghanath on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HistoryBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
